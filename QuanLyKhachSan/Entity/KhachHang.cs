f8146d62df59577b243431f486856f5c2bc2bd49
NyenT <vbtnguyen2k6@gmail.com>
2026-07-03 15:54:18 +0700

    Add project files.

---
+++ QuanLyKhachSan/Entity/KhachHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachSan.Entity
+{
+    public class KhachHang
+    {
+        public string MaKH { get; set; }
+        public string HoTen { get; set; }
+        public string CCCD { get; set; }
+        public string SoDienThoai { get; set; }
+        public string DiaChi { get; set; }
+    }
+}
